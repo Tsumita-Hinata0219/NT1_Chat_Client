c0c6794dff0bdefa1296305f7412969b05f20681
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2025-01-19 23:20:26 +0900

    save

--- main.cpp
+++ main.cpp
@@ -8,7 +8,18 @@
 
 int main()
 {
+	/* WinSockの初期化 */
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		printf("WSAStartupに失敗しました。エラーコード: %d\n", WSAGetLastError());
+		return 1;
+	}
 
+
+
+
+
+	WSACleanup();
 	return 0;
 }
 
